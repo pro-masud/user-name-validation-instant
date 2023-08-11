5c76d24f103a66184d56d6ebb70556b49f5952b0
pro-mannan <promasudbd@gmail.com>
2023-08-11 23:03:20 +0600

    complete username validation

--- app/main.js
+++ app/main.js
@@ -11,8 +11,12 @@ input.onkeyup = (e) => {
   const data = e.target.value;
   
   // user validation here 
-  if(data.length > 0 && data.length < 5 ){
-    msg.innerHTML = "";
+  if(data.length > 0 && data.length <= 5 ){
+    msg.innerHTML = '<span style="color: skyblue;">Plz Simble Minimam 5 Character</span>';
+  }else if(data.length > 5 && data.length <= 12){
+    msg.innerHTML = '<span style="color: green;">You are Right Now !</span>';
+  }else{
+    msg.innerHTML = '<span style="color: red;">Incurrect User Name</span>';
   }
 
 }
